ee7335dddb35887a78252037b12aab4cebf3ec0e
Nisha Dubalgunde <nishadubalgunde@gmail.com>
2026-03-17 12:14:39 +0530

    Implement largest element finder in largest_element.cpp
    Add logic to find the largest element in an array.

--- Basics/Patterns/Arrays/largest_element.cpp
+++ Basics/Patterns/Arrays/largest_element.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std ; 
+int main (){
 
+    int arr [] = { 3 , 7 ,2 ,9 ,5};
+    int n = 5 ;
+    int largest = arr [0] ; 
+
+    for (int i = 1 ; i < n ; i++){
+        if (arr [i] > largest){
+            largest = arr[i];
+
+        }
+    }
+    cout <<"largest element is :" <<largest;
+
+    return 0 ;
+
+}
