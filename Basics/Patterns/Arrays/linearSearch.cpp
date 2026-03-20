ae89d85bcd8d4a3e88531ff2f2ce319a93d54182
Nisha Dubalgunde <nishadubalgunde@gmail.com>
2026-03-20 21:43:05 +0530

    Implement linear search and array modification functions
    Added a function to modify array elements and a linear search function to find an element's index.

--- Basics/Patterns/Arrays/linearSearch.cpp
+++ Basics/Patterns/Arrays/linearSearch.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std; 
 
+void changeArr(int arr[], int size) {
+    cout << "in function\n";
+    for (int i = 0; i < size; i++) {
+        arr[i] = 2 * arr[i]; 
+    }
+}
+
+int linearSearch(int arr[], int sz, int target) {
+    for (int i = 0; i < sz; i++) {
+        if (arr[i] == target) {
+            return i;  // return index when found
+        }
+    }
+    return -1;  // not found
+}
+
+int main() {
+    int arr[] = {4, 2, 7, 8, 2, 2, 5};
+    int sz = 7; 
+    int target = 8;
+
+    cout << linearSearch(arr, sz, target) << endl;
+    return 0;
+}
